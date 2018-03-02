4a86a0f8a01bf2a48af22aa497ac42dd4795c99b
cmoore4os <cm4glos@gmail.com>
2018-03-02 14:14:01 -0500

    Add answers.md.; corrected some errors in cpu.js, but still incomplete

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -5,21 +5,52 @@
 const fs = require('fs');
 
 // Instructions
+
+// General registers
+const IM = 0b00000101;
+const IS = 0b00000110;
+const SP = 0b00000111;
+
+// Flag values
+const FL_EQ = 0b00000001; // regA = regB
+const FL_GT = 0b00000010; // regA > regB
+const FL_LT = 0b00000100; // regA < regB
+
 const ADD = 0b10101000; // adds regA and regB
 const AND = 0b10110011; // bitwise& regA and regB
+const CALL = 0b01001000; // CALL R
+const CMP = 0b10100000; //
+
 const DEC = 0b01111001; // decreases a register by 1
 const DIV = 0b10101011; // divides regA by regB; return error if regB === 0
 const HLT = 0b00000001; // Halt CPU
 const INC = 0b01111000; // increases a register by 1
+const INT = 0b01001010; // Software interrupt R
+const IRET = 0b00001011; // Return from interrupt
 
+const JEQ = 0b01010001; // JEQ R
+const JGT = 0b01010100; // JGT R
+const JLT = 0b01010011; // JLT R
+const JMP = 0b01010000; // JMP R
+const JNE = 0b01010010; // JNE R
+
+const LD = 0b10011000; // set regA with the value stored at the address stored in regB
 const LDI = 0b10011001; // LDI R,I(mmediate)
-
+const MOD = 0b10101100; // Remainder of regA divided by regB
 const MUL = 0b10101010; // multiply regA by regB
+const NOP = 0b00000000; // NOP no operation; do nothing
+const NOT = 0b01110000; // NOT R
 
+const OR = 0b10110001; // OR regA and regB
+const POP = 0b01001100; // Pop register off the stack
+const PRA = 0b01000010; // Print alpha char to regA to the console
 const PRN = 0b01000011; // prints numeric value in regA to the console
+const PUSH = 0b01001101; // Push a reg on to stack
 
+const RET = 0b00001001; // Return from subroutine
+const ST = 0b10011010; // Store regB in the address stored in regA
 const SUB = 0b10101001; // subtracts regB from regA
-
+const XOR = 0b10110010; // bitwise-XOR regA and regB
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
@@ -48,14 +79,39 @@ class CPU {
 
     bt[ADD] = this.ADD;
     bt[AND] = this.AND;
+    bt[CALL] = this.CALL;
+    bt[CMP] = this.CMP;
+
     bt[DEC] = this.DEC;
     bt[DIV] = this.DIV;
     bt[HLT] = this.HLT;
     bt[INC] = this.INC;
+    bt[INT] = this.INT;
+    bt[IRET] = this.IRET;
+
+    bt[JEQ] = this.JEQ;
+    bt[JGT] = this.JGT;
+    bt[JLT] = this.JLT;
+    bt[JMP] = this.JMP;
+    bt[JNE] = this.JNE;
+
+    bt[LD] = this.LD;
     bt[LDI] = this.LDI;
+    bt[MOD] = this.MOD;
     bt[MUL] = this.MUL;
+    bt[NOP] = this.NOP;
+    bt[NOT] = this.NOT;
+
+    bt[OR] = this.OR;
+    bt[POP] = this.POP;
+    bt[PRA] = this.PRA;
     bt[PRN] = this.PRN;
+    bt[PUSH] = this.PUSH;
+
+    bt[RET] = this.RET;
+    bt[ST] = this.ST;
     bt[SUB] = this.SUB;
+    bt[XOR] = this.XOR;
 
     this.branchTable = bt;
   }
@@ -83,6 +139,19 @@ class CPU {
    */
   stopClock() {
     clearInterval(this.clock);
+  }
+
+  /**
+   * Set Flag
+   */
+  setFlag(flag, value) {
+    if (value === true) {
+      //Set the Flag
+      this.reg.FL = this.reg.FL | flag;
+    } else {
+      // Clear the Flag to 0
+      this.reg.FL = this.reg.FL & ~flag;
+    }
   }
 
   /**
@@ -135,16 +204,6 @@ class CPU {
     // Based on the value in the Instruction Register, locate the
     // appropriate hander in the branchTable
     // !!! IMPLEMENT ME
-    let handler = this.branchTable[this.reg.IR];
-
-    // Check that the handler is defined, halt if not (invalid
-    // instruction)
-    // !!! IMPLEMENT ME
-    if (handler === undefined) {
-      console.error('Unknown opcode ' + this.reg.IR);
-      this.stopClock();
-      return;
-    }
 
     // Read operandA and operandB
 
@@ -186,41 +245,107 @@ class CPU {
     // Call the ALU
     this.alu('ADD', regA, regB);
   }
-  SUB(regA, regB) {
-    // !!! IMPLEMENT ME
-    // Call the ALU
-    this.alu('SUB', regA, regB);
+
+  CALL(regA) {
+    this._push(this.reg.PC + 2);
+    return this.reg[regA];
   }
-  MUL(regA, regB) {
-    // !!! IMPLEMENT ME
-    // Call the ALU
-    this.alu('MUL', regA, regB);
-  }
-  DIV(regA, regB) {
-    // !!! IMPLEMENT ME
-    // Call the ALU
-    this.alu('DIV', regA, regB);
+  CMP(regA, regB) {
+    this.alu('CMP', regA, regB);
   }
   DEC(regA, regB) {
     // !!! IMPLEMENT ME
     // Call the ALU
     this.alu('DEC', regA, regB);
   }
+
+  DIV(regA, regB) {
+    // !!! IMPLEMENT ME
+    // Call the ALU
+    this.alu('DIV', regA, regB);
+  }
+  INC(regA, regB) {
+    // !!! IMPLEMENT ME
+    // Call the ALU
+    this.alu('INC', regA, regB);
+  }
+  JEQ(regA) {
+    if ((this.reg.FL & FL_EQ) === 1) {
+      return this.reg[regA];
+    }
+  }
+
+  JGT(regA) {
+    if ((this.reg.FL & FL_GT) === 2) {
+      return this.reg[regA];
+    }
+  }
+
+  JLT(regA) {
+    if ((this.reg.FL & FL_LT) === 4) {
+      return this.reg[regA];
+    }
+  }
+
+  JMP(regA) {
+    return this.reg[regA];
+  }
+
+  JNE(regA) {
+    if ((this.reg.FL & FL_EQ) === 0) {
+      return this.reg[regA];
+    }
+  }
+  LD(regA, regB) {
+    this.reg[regA] = this.ram.read(this.reg[regB]);
+  }
+
+  MOD(regA, regB) {
+    this.alu('MOD', regA, regB);
+  }
   MUL(regA, regB) {
     // !!! IMPLEMENT ME
     // Call the ALU
     this.alu('MUL', regA, regB);
   }
+  NOT(regA) {
+    this.alu('NOT', regA);
+  }
 
-  /**
-   * PRN R
-   */
+  OR(regA, regB) {
+    this.alu('OR', regA, regB);
+  }
+
+  POP(regA) {
+    this.reg[regA] = this._pop();
+  }
+
+  PRA(regA) {
+    const value = this.reg[regA];
+    console.log(String.fromCharCode(value));
+  }
+
   PRN(regA) {
     console.log(parseInt(this.reg[regA]));
   }
+  PUSH(regA) {
+    this._push(this.reg[regA]);
+  }
 
+  RET() {
+    return this._pop();
+  }
+
+  ST(regA, regB) {
+    const value = this.reg[regB];
+    this.ram.write(this.reg[regA], value);
+  }
   SUB(regA, regB) {
-    // !!! IMPLEMENT ME
+    this.alu('SUB', regA, regB);
+  }
+
+  XOR(regA, regB) {
+    this.alu('XOR', regA, regB);
   }
 }
 
